efb7400f3f7a64ee29702afa2e4d2c81a8c42f77
Guillem <guillemus@proton.me>
2024-11-02 19:21:00 -0300

    formatting

--- scripts/testclient.ts
+++ scripts/testclient.ts
@@ -1,25 +1,25 @@
-import { createApiClient } from "./apiclient"
+import { createApiClient } from './apiclient'
 
-const apiclient = createApiClient("http://localhost:8080", {
+const apiclient = createApiClient('http://localhost:8080', {
   beforeRequest(config) {
     if (config?.headers) {
-      config.headers["Authorization"] = "lol"
+      config.headers['Authorization'] = 'lol'
     }
   },
 })
 
-console.log(await apiclient.main.HelloWorld({ name: "asdfa" }))
+console.log(await apiclient.main.HelloWorld())
 console.log(
   await apiclient.main.ExampleHandler1({
-    name: "name",
-    users: [{ name: "name", age: 0 }],
-  })
+    name: 'name',
+    users: [{ name: 'name', age: 0 }],
+  }),
 )
 console.log(
   await apiclient.main.ExampleHandler2({
-    name: "name",
-    users: [{ name: "name", age: 0 }],
-  })
+    name: 'name',
+    users: [{ name: 'name', age: 0 }],
+  }),
 )
-console.log(await apiclient.pkg.SomeHandler({}))
-console.log(await apiclient.main.getPlaylists({}))
+console.log(await apiclient.pkg.SomeHandler())
+console.log(await apiclient.main.getPlaylists())
